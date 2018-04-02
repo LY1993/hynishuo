53892cfd43a9878d2ee21d33ad1694e0d58682ee
root <634371950@qq.com>
2018-04-02 23:23:59 +0800

    public port

--- config/index.js
+++ config/index.js
@@ -20,9 +20,10 @@ module.exports = {
 
     // Various Dev Server settings
     host: 'localhost', // can be overwritten by process.env.HOST
-    port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
+    port: 8888, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
+    public: '123.207.64.115:8080',
     notifyOnErrors: true,
     poll: false, // https://webpack.js.org/configuration/dev-server/#devserver-watchoptions-
 
